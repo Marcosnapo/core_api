d9b9d8eac297dacfaaf3dda9dc19d26dc8ebf390
Marcosnapo <marcos.sebastian.napolitano@gmail.com>
2025-08-06 15:07:40 -0300

    Update views.py

--- projects/views.py
+++ projects/views.py
@@ -5,3 +5,11 @@ from .models import Project
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    # --- Cebo de diagnóstico ---
+    def list(self, request, *args, **kwargs):
+        print("--- Cebo activado: Solicitud GET recibida en el backend ---")
+        return super().list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+        print("--- Cebo activado: Solicitud POST recibida en el backend ---")
+        return super().create(request, *args, **kwargs)
